a9c54bff8b622acbe586cee8923bbb63ee4209b1
Krishnauprit18 <krishna.collage.official@gmail.com>
2026-09-18 06:20:30 +0530

    feat: add Floci Terraform infrastructure and remote state

---
+++ infra/terraform/modules/iam/main.tf
@@ -0,0 +1,18 @@
+resource "aws_iam_role" "ciis_app" {
+  name = "ciis-app-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+output "role_name" {
+  value = aws_iam_role.ciis_app.name
+}
