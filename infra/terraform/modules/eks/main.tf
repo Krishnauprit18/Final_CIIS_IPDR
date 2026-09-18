e90fb5a8ec0b6c3b249b2f69bf8108d7aa89b2c3
Krishna Uprit <krishna.collage.official@gmail.com>
2026-09-18 07:44:54 +0530

    fix: stabilize local Floci EKS and Terraform configuration

--- infra/terraform/modules/eks/main.tf
+++ infra/terraform/modules/eks/main.tf
@@ -6,9 +6,14 @@ resource "aws_eks_cluster" "ciis" {
   name     = "ciis-local"
   role_arn = var.cluster_role_arn
 
+  # Floci seeds these default subnets in every region. Using real seeded IDs
+  # satisfies the AWS provider schema and Floci's EKS validation without
+  # inventing a subnet that does not exist.
   vpc_config {
-    subnet_ids         = []
-    security_group_ids = []
+    subnet_ids = [
+      "subnet-default-a",
+      "subnet-default-b",
+    ]
   }
 }
 
@@ -18,4 +23,8 @@ output "cluster_name" {
 
 output "endpoint" {
   value = aws_eks_cluster.ciis.endpoint
+}
+
+output "status" {
+  value = aws_eks_cluster.ciis.status
 }
