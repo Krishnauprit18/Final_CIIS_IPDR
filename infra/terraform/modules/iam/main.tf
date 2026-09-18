2b8deca989f41370e7fd739f843732443464d888
Krishna Uprit <krishna.collage.official@gmail.com>
2026-09-18 07:44:50 +0530

    fix: stabilize local Floci EKS and Terraform configuration

--- infra/terraform/modules/iam/main.tf
+++ infra/terraform/modules/iam/main.tf
@@ -13,10 +13,29 @@ resource "aws_iam_role" "ciis_app" {
   })
 }
 
+resource "aws_iam_role" "eks_cluster" {
+  name = "ciis-eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
 output "role_name" {
   value = aws_iam_role.ciis_app.name
 }
 
 output "role_arn" {
   value = aws_iam_role.ciis_app.arn
+}
+
+output "eks_cluster_role_arn" {
+  value = aws_iam_role.eks_cluster.arn
 }
